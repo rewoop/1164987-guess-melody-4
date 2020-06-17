7f0cd3b039140abfa8247e857aad5061ade18c0c
Sergey <10rubleqow@gmail.com>
2020-06-17 18:09:57 +0300

    fix

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -5,6 +5,7 @@ import {GameType} from "../../const.js";
 class GenreQuestionScreen extends PureComponent {
   constructor(props) {
     super(props);
+
     this.state = {
       answers: [false, false, false, false],
     };
@@ -27,7 +28,8 @@ class GenreQuestionScreen extends PureComponent {
           </a>
 
           <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-            <circle className="timer__line" cx="390" cy="390" r="370" style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
+            <circle className="timer__line" cx="390" cy="390" r="370"
+              style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
           </svg>
 
           <div className="game__mistakes">
